1adeddbb1b5474d0b9c6c1a068fb7ab746af60aa
CODE WRANGLER <33333881+J-Bonwick@users.noreply.github.com>
2020-09-02 00:25:48 +1000

    Implemented NoticeDisplay Model

---
+++ DigitalNoticeBoard/Models/NoticeDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalNoticeBoard.Models
+{
+    public class NoticeDisplay
+    {
+        public int NoticeDisplayID { get; set; }
+        [Display(Name = "Notice Display Name")]
+        public string NoticeBoardName { get; set; }
+
+        public ICollection<NoticeAssign> NoticeAssignments { get; set; }
+    }
+}
