13f7c94e50745a270ba7ffa8b8f06d2c69472f45
CODE WRANGLER <33333881+J-Bonwick@users.noreply.github.com>
2020-09-02 00:26:14 +1000

    Implemented NoticeAssign Model

--- DigitalNoticeBoard/Models/NoticeAssign.cs
+++ DigitalNoticeBoard/Models/NoticeAssign.cs
@@ -7,5 +7,10 @@ namespace DigitalNoticeBoard.Models
 {
     public class NoticeAssign
     {
+        public int NoticeAssignID { get; set; }
+        public int NoticeID { get; set; }
+        public Notice Notice { get; set; }
+        public int NoticeDisplayID { get; set; }
+        public NoticeDisplay NoticeDisplay { get; set; }
     }
 }
