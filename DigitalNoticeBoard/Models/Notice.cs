884367006e785ce893c787489889b8565fc687d6
CODE WRANGLER <33333881+J-Bonwick@users.noreply.github.com>
2020-09-02 00:26:25 +1000

    Implemented Notice Model

--- DigitalNoticeBoard/Models/Notice.cs
+++ DigitalNoticeBoard/Models/Notice.cs
@@ -7,5 +7,15 @@ namespace DigitalNoticeBoard.Models
 {
     public class Notice
     {
+        public int NoticeID { get; set; }
+        public string NoticeName { get; set; }
+        //public byte[] NoticeImage { get; set; }
+        public DateTime NoticeStartDate { get; set; }
+        public DateTime NoticeStopDate { get; set; }
+        public String NoticeImagePath { get; set; }
+
+
+
+        public ICollection<NoticeAssign> NoticeAssignments { get; set; }
     }
 }
