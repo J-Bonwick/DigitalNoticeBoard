682ce41d91b20026cebfb0540f810ec4d7aabafb
CODE WRANGLER <33333881+J-Bonwick@users.noreply.github.com>
2020-09-02 16:14:13 +1000

    Added db context

---
+++ DigitalNoticeBoard/Data/DigitalNoticeBoardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DigitalNoticeBoard.Models;
+
+namespace DigitalNoticeBoard.Data
+{
+    public class DigitalNoticeBoardContext : DbContext
+    {
+        public DigitalNoticeBoardContext (DbContextOptions<DigitalNoticeBoardContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DigitalNoticeBoard.Models.Notice> Notice { get; set; }
+    }
+}
