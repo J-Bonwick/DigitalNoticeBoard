77b004a8fbcdd830e178891b816ab5deda49d090
CODE WRANGLER <33333881+J-Bonwick@users.noreply.github.com>
2020-09-16 22:03:20 +1000

    Added [Required] to the notice name

--- DigitalNoticeBoard/Models/Notice.cs
+++ DigitalNoticeBoard/Models/Notice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace DigitalNoticeBoard.Models
     public class Notice
     {
         public int NoticeID { get; set; }
+        [Required]
         public string NoticeName { get; set; }
         //public byte[] NoticeImage { get; set; }
         public DateTime NoticeStartDate { get; set; }
