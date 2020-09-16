4230b59ab00d3e58b3543af171d2e10eeabd6011
CODE WRANGLER <33333881+J-Bonwick@users.noreply.github.com>
2020-09-16 15:14:24 +1000

    Implemented dynamic image generation

--- DigitalNoticeBoard/Pages/Index.cshtml.cs
+++ DigitalNoticeBoard/Pages/Index.cshtml.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DigitalNoticeBoard.Data;
+using DigitalNoticeBoard.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DigitalNoticeBoard.Pages
@@ -11,15 +14,27 @@ namespace DigitalNoticeBoard.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly DigitalNoticeBoard.Data.DigitalNoticeBoardContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty(SupportsGet = true)]
+        public string displayID { get; set; }
+        public IList<NoticeAssignment> NoticeAssignment { get; set; }
+
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IndexModel(DigitalNoticeBoard.Data.DigitalNoticeBoardContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            NoticeAssignment = await _context.NoticeAssignments
+                .Include(n => n.Notice)
+                .Include(n => n.NoticeDisplay).ToListAsync();
         }
     }
 }
