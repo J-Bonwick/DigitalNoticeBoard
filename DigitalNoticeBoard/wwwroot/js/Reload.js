9a6006be9358427c7615c4b868bda46c0319843b
CODE WRANGLER <33333881+J-Bonwick@users.noreply.github.com>
2020-09-16 22:01:19 +1000

    Added Reconnect when the client disconnected

--- DigitalNoticeBoard/wwwroot/js/Reload.js
+++ DigitalNoticeBoard/wwwroot/js/Reload.js
@@ -1,10 +1,30 @@
-﻿var connection = new signalR.HubConnectionBuilder().withUrl("/reload").build();
-connection.on("Reload", function () {
-    location.reload(true);
+﻿
+var connection = new signalR.HubConnectionBuilder().withUrl("/reload").build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("connected");
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start(), 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
 });
 
-connection
-    .start()
-    .catch(function (err) {
-        return console.error(err.toString());
-    });
+start();
+
+
+
+//connection.on("Reload", function () {
+//    location.reload(true);
+//});
+
+//connection
+//    .start()
+//    .catch(function (err) {
+//        return console.error(err.toString());
+//    });
