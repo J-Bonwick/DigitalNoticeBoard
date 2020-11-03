df8a7693e3c368be738120c77874cfe81631d7d4
CODE WRANGLER <jack@bonwick.dev>
2020-11-04 00:52:36 +1100

    Implemented image deletion

--- DigitalNoticeBoard/Pages/Manage/Notices/Delete.cshtml.cs
+++ DigitalNoticeBoard/Pages/Manage/Notices/Delete.cshtml.cs
@@ -8,18 +8,22 @@ using Microsoft.EntityFrameworkCore;
 using DigitalNoticeBoard.Data;
 using DigitalNoticeBoard.Models;
 using Microsoft.AspNetCore.SignalR;
+using System.IO;
+using Microsoft.Extensions.Hosting;
 
 namespace DigitalNoticeBoard.Pages.Manage.Notices
 {
     public class DeleteModel : PageModel
     {
         private readonly DigitalNoticeBoard.Data.DigitalNoticeBoardContext _context;
+        private readonly IHostEnvironment hostingEnvironment;
         private readonly IHubContext<ReloadHub> _hubContext;
 
-        public DeleteModel(DigitalNoticeBoard.Data.DigitalNoticeBoardContext context, IHubContext<ReloadHub> hubContext)
+        public DeleteModel(DigitalNoticeBoard.Data.DigitalNoticeBoardContext context, IHubContext<ReloadHub> hubContext, IHostEnvironment environment)
         {
             _context = context;
             _hubContext = hubContext;
+            this.hostingEnvironment = environment;
         }
 
         [BindProperty]
@@ -54,6 +58,13 @@ namespace DigitalNoticeBoard.Pages.Manage.Notices
             {
                 _context.Notices.Remove(Notice);
                 await _context.SaveChangesAsync();
+            }
+            //Delete the local file
+            var dir = Path.Combine(hostingEnvironment.ContentRootPath, "wwwroot/imageUpload");
+            var filePath = Path.Combine(dir, Notice.NoticeImagePath);
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
             }
 
             await _hubContext.Clients.All.SendAsync("Reload");
